b86b2da3672096420d6fc33db13374661dbead5f
Cardona <carlos.cardona@softwareone.com>
2022-08-22 19:48:06 -0500

    Commit del ejercicio del return correción

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,14 +28,16 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  var provista = str;
+  return provista;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  var suma = x + y;
+  return suma;
 }
 
 function resta(x, y) {
